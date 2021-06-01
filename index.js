2d7a9f96dbc622badcdd6e0a1bc4d7959ec75f3f
Tushar Ojha <tusharojha2001@gmail.com>
2021-06-01 20:21:30 +0530

    changed react to basic html client

--- index.js
+++ index.js
@@ -2,21 +2,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// Project Imports
+const routes = require("./routes.js");
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 // Routes here
+app.use("/api", routes);
+
+// Set static folder
+app.use(express.static("client"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
